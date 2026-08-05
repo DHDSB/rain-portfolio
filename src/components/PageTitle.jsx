3bf0637d3f8cea9a1f8e45d5788b7cc2a397e116
Rain Tan <rain.tan@calix.com>
2026-08-05 16:46:20 +0800

    修改PageTitle.jsx，确保显示正确的浏览器标签标题

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,19 +1,39 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+import { contentItems } from "../data/content.js";
+import { siteConfig } from "../data/siteConfig.js";
+
 const pageTitles = {
-  "/": "Rain Tan | 首页",
-  "/works": "作品 | Rain Tan",
-  "/writing": "内容 | Rain Tan",
-  "/about": "关于 | Rain Tan",
+  "/": "首页",
+  "/works": "作品",
+  "/writing": "内容",
+  "/about": "关于",
 };
 
 export default function PageTitle() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    document.title =
-      pageTitles[pathname] || "页面不存在 | Rain Tan";
+    if (pathname.startsWith("/content/")) {
+      const contentId = pathname.split("/content/")[1];
+
+      const item = contentItems.find(
+        (contentItem) => contentItem.id === contentId
+      );
+
+      document.title = item
+        ? `${item.title} | ${siteConfig.name}`
+        : `内容不存在 | ${siteConfig.name}`;
+
+      return;
+    }
+
+    const pageTitle = pageTitles[pathname];
+
+    document.title = pageTitle
+      ? `${pageTitle} | ${siteConfig.name}`
+      : `页面不存在 | ${siteConfig.name}`;
   }, [pathname]);
 
   return null;
